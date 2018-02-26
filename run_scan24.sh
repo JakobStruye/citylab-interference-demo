8fbb24447eca5d9ccd216d9de5414fccb75738a0
Jakob Struye <git@jstr.be>
2018-02-26 13:30:29 +0100

    Updated script to alternatingly scan 2.4 and 5

--- run_scan24.sh
+++ run_scan24.sh
@@ -42,22 +42,29 @@ until ip link set dev $devname up; do : ; done;
 #echo "Reset $devname"
 PREVTIME=$(date +%s%N)
 NEXT=$(date +%s%N)
+DOFIVE=true
 while true
 do
+    INTERVAL=5000000000
     TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S.%3N")
     NOW=$(date +%s%N)
-    
+
     if [ $NOW -lt $NEXT ]
     then
-        sleep $(bc -l <<< $(( $NEXT - $NOW ))/1000000000)
+        sleep $(bc -l <<< $(( $NEXT - $NOW ))/$INTERVAL)
     fi
-    NEXT=$(( $NEXT + 100000000 ))
+    NEXT=$(( $NEXT + $INTERVAL ))
     echo background > $ATHPATH/spectral_scan_ctl
     echo trigger > $ATHPATH/spectral_scan_ctl
     #echo "Set pre-scan values; scanning..."
     #/sbin/iw dev $devname scan freq 2412 2417 2422 2427 2432 2437 2442 2447 2452 2457 2462 & kill $! #Will probably fail
     #echo "Scan 1 complete"
-    /sbin/iw dev $devname scan freq 2412 &> /dev/null #2417 2422 2427 2432 2437 2442 2447 2452 2457 2462 #Will probably work
+    if [ "$DOFIVE" = true] ; then
+      /sbin/iw dev wls6 scan freq 5180 5200 5220 5240 5260 5280 5300 5320 5500 5520 5540 5560 5580 5600 5620 5640 5660 5680 5700
+    else
+      /sbin/iw dev $devname scan freq 2412 2417 2422 2427 2432 2437 2442 2447 2452 2457 2462
+    fi
+     #Will probably work
     #echo "Scan 2 complete"
     echo disable > $ATHPATH/spectral_scan_ctl
     #echo "Spectral scan disabled"
@@ -68,4 +75,3 @@ do
     #sleep $(bc -l <<< $(( 100000000 - $(( $(date +%s%N) - $PREVTIME )) ))/1000000000) &> /dev/null
     PREVTIME=$NEWTIME
 done
-
