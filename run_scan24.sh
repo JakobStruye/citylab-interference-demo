a3d92845767016525cb0e9a17b5143e1ef8a237c
Jakob Struye <jakob.struye@uantwerpen.be>
2018-02-26 08:32:08 -0700

    Further additions to alternating scan

--- run_scan24.sh
+++ run_scan24.sh
@@ -45,28 +45,28 @@ NEXT=$(date +%s%N)
 DOFIVE=true
 while true
 do
-    INTERVAL=5000000000
+    TOSECOND=1000000000
+    INTERVAL=3
     TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S.%3N")
     NOW=$(date +%s%N)
 
     if [ $NOW -lt $NEXT ]
     then
-        sleep $(bc -l <<< $(( $NEXT - $NOW ))/$INTERVAL)
+        sleep $(bc -l <<< $(( $NEXT - $NOW ))/$TOSECOND)
+	#echo $(bc -l <<< $(( $NEXT - $NOW ))/$TOSECOND)
     fi
-    NEXT=$(( $NEXT + $INTERVAL ))
+    NEXT=$(( $NEXT + $TOSECOND * $INTERVAL ))
     echo background > $ATHPATH/spectral_scan_ctl
     echo trigger > $ATHPATH/spectral_scan_ctl
     #echo "Set pre-scan values; scanning..."
     #/sbin/iw dev $devname scan freq 2412 2417 2422 2427 2432 2437 2442 2447 2452 2457 2462 & kill $! #Will probably fail
     #echo "Scan 1 complete"
     if [ "$DOFIVE" = true ] ; then
-      /sbin/iw dev wls6 scan freq 5180 5200 5220 5240 5260 5280 5300 5320 5500 5520 5540 5560 5580 5600 5620 5640 5660 5680 5700 &> /dev/null
+      /sbin/iw dev $devname scan freq 5180 5200 5220 5240 5260 5280 5300 5320 5500 5520 5540 5560 5580 5600 5620 5640 5660 5680 5700 &> /dev/null
       DOFIVE=false
-      echo "Scanned 2.4"
     else
       /sbin/iw dev $devname scan freq 2412 2417 2422 2427 2432 2437 2442 2447 2452 2457 2462 &> /dev/null
       DOFIVE=true
-      echo "Scanned 5"
     fi
      #Will probably work
     #echo "Scan 2 complete"
